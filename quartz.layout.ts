3b12e87e5f3169bdd4cca2a15d8d885f8e14a39e
phungminhchi2k5 <phungminhchi2005@gmail.com>
2026-01-01 23:50:33 +0700

    temp hide git

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/chisphung",
+      // GitHub: "https://github.com/chisphung",
+       GitHub: "https://github.com/"
       // "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
